a493780c94c0755a45ca346f0af0f680d4710ec5
YuvaSaiP <51235709+YuvaSaiP@users.noreply.github.com>
2019-10-29 12:32:05 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
 // Future JavaScript will go here
+// I'm writing some code to display the source code of this extension in webpage
+
+// alert('hello')
+
+document.documentElement.innerHTML =
+  "<pre>" + document.documentElement.innerHTML.replace(/</g, "&lt;") + "</pre>";
